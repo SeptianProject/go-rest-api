fc7e6cf65180b39f6477a72344079ae0522bdc00
Septian Asropik <septianuwoe75@gmail.com>
2025-02-24 01:30:43 +0700

    feat: create book restful api

--- go.mod
+++ go.mod
@@ -2,8 +2,11 @@ module go-rest-api
 
 go 1.24.0
 
+require github.com/gorilla/handlers v1.5.2
+
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-sql-driver/mysql v1.9.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,143 @@
 package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+
+type Book struct {
+	gorm.Model 	`json:"-"`
+	ID			uint    `json:"id"`
+	Title     	string  `json:"title"`
+	Author    	string  `json:"author"`
+	Price     	float64 `json:"price"`
+	Available 	bool    `json:"available" gorm:"default:true"`
+	CreatedAt 	time.Time  `json:"created_at"`
+	UpdatedAt 	time.Time  `json:"updated_at"`
+	DeletedAt 	time.Time  `json:"deleted_at,omitempty"`
+}
+
+var db *gorm.DB
+var err error
+
+func InitDB(){
+	dsn := "root:@tcp(127.0.0.1:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	} else {
+		log.Println("Successfully connected to database!")
+	}
+	db.AutoMigrate(&Book{})
+}
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var books []Book
+	result := db.Find(&books)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(books)
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var book Book
+	result := db.First(&book, params["id"])
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result:=db.Create(&book)
+	if result.Error !=nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var book Book
+	if result := db.First(&book, params["id"]); result.Error != nil  {
+		http.Error(w, result.Error.Error(), http.StatusNotFound)
+		return
+	}
+
+	var updateBook Book
+	if err := json.NewDecoder(r.Body).Decode(&updateBook); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	db.Model(&book).Updates(updateBook)
+	json.NewEncoder(w).Encode(book)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var book Book
+	if result := db.First(&book, params["id"]); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusNotFound)
+		return
+	}
+
+	db.Delete(&book)
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+func main() {
+	InitDB()
+
+	r:= mux.NewRouter()
+	r.Use(loggingMiddleware)
+
+	r.HandleFunc("/api/books", GetBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", GetBook).Methods("GET")
+	r.HandleFunc("/api/books", CreateBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", UpdateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", DeleteBook).Methods("DELETE")
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+	
+	log.Println("Server started on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(originsOk, headersOk, methodsOk)(r)))
+}
